d7f68836506a4bd02a68c04f2383933fbacdc89b
rashed081 <rashed.khan.rony.me@gmail.com>
2025-09-12 09:20:37 +0000

    restructure code

--- pulumi_project/__main__.py
+++ pulumi_project/__main__.py
@@ -61,7 +61,8 @@ pulumi.export("nat_eip_id",nat_eip.id)
 nat_gw = aws.ec2.NatGateway(
     "nat_gw",
     allocation_id = nat_eip.id,
-    subnet_id = public_subnet.id
+    subnet_id = public_subnet.id,
+    tags={'Name': 'nat_gw'}
 )
 pulumi.export("nat_gw_id", nat_gw.id)
 
@@ -136,15 +137,92 @@ bastion_sg = aws.ec2.SecurityGroup(
 pulumi.export("bastion_sg_id", bastion_sg.id)
 
 
+
+private_sg = aws.ec2.SecurityGroup(
+    "private_sg",
+    vpc_id = vpc.id,
+    description ="Allow ssh only from Public SG.",
+    ingress=[
+        aws.ec2.SecurityGroupIngressArgs(
+            protocol='tcp',
+            from_port=22,
+            to_port=22,
+            cidr_blocks=[public_subnet.cidr_block],
+        ),
+        aws.ec2.SecurityGroupIngressArgs(
+            protocol='tcp',
+            from_port=3306,
+            to_port=3306,
+            cidr_blocks=[public_subnet.cidr_block],
+        ),
+    ],
+    egress =[
+        aws.ec2.SecurityGroupEgressArgs(
+            protocol ="-1",
+            from_port=0,
+            to_port=0,
+            cidr_blocks = ["0.0.0.0/0"]
+        )
+    ],
+    tags={"Name":"private_sg"}
+)
+
+
+with open('../script/mysql-setup.sh', 'r') as file:
+    print("Reading MySQL setup script...\n")
+    mysql_setup_script = file.read()
+
+def generate_mysql_user_data():
+    return f'''#!/bin/bash
+exec > >(tee /var/log/user-data.log) 2>&1
+
+apt-get update
+apt-get upgrade -y
+mkdir -p /usr/local/bin
+cat > /usr/local/bin/mysql-setup.sh << 'EOL'
+{mysql_setup_script}
+EOL
+
+chmod +x /usr/local/bin/mysql-setup.sh
+/usr/local/bin/mysql-setup.sh
+'''
+db_server = aws.ec2.Instance(
+    'db-server',
+    instance_type='t2.small',
+    ami =ami_id ,
+    subnet_id = private_subnet.id,
+    key_name="MyKeyPair",
+    vpc_security_group_ids =[private_sg.id],
+    user_data=generate_mysql_user_data(),
+    user_data_replace_on_change=True,
+    tags={'Name': 'db-server'},
+    opts=pulumi.ResourceOptions(
+        depends_on=[
+            nat_gw,
+            private_subnet
+        ]
+    )
+)
+
+pulumi.export("db_server_ip", db_server.private_ip)
+
 with open("MyKeyPair.pub") as f:
 	    ssh_pub_key = f.read().strip()
         
-bastion_user_data = f"""#!/bin/bash
+def generate_bastion_user_data(db_private_ip):
+    return f"""#!/bin/bash
+exec > >(tee /var/log/user-data.log) 2>&1
 set -euxo pipefail
+apt-get update
+apt-get install -y git
+echo "DB_PRIVATE_IP={db_private_ip}" >> /etc/environment
+source /etc/environment
+git clone https://github.com/rashed081/aws_resources_with_pulumi.git /tmp
+chmod +x /tmp/aws_resources_with_pulumi/scripts/setup.sh
+bash /tmp/aws_resources_with_pulumi/scripts/setup.sh
 
 useradd -m -s /bin/bash ops || true
-usermod -aG sudo ops || true   # <-- use 'sudo' instead of 'wheel' on Ubuntu
-
+usermod -aG sudo ops || true
 mkdir -p /home/ops/.ssh
 cat > /home/ops/.ssh/authorized_keys <<'PUBKEY'
 {ssh_pub_key}
@@ -167,50 +245,34 @@ systemctl restart sshd
 bastion_server = aws.ec2.Instance(
     "bastion_server",
     ami =ami_id,
-    instance_type = "t2.micro",
+    instance_type = "t2.small",
     subnet_id = public_subnet.id,
     vpc_security_group_ids =[bastion_sg.id],
     associate_public_ip_address = True,
     key_name="MyKeyPair",
-    user_data = bastion_user_data,
+    user_data=pulumi.Output.all(db_server.private_ip).apply(
+        lambda args: generate_bastion_user_data(args[0])
+    ),
     tags={"Name": "bastion_server"}
 )
 pulumi.export("bastion_server_ip", bastion_server.public_ip)
 
+def create_config_file(all_ips):
+    config_content = f"""Host bastion-server
+    HostName {all_ips[0]}
+    User ops
+    IdentityFile ~/.ssh/MyKeyPair.id_rsa
 
-private_sg = aws.ec2.SecurityGroup(
-    "private_sg",
-    vpc_id = vpc.id,
-    description ="Allow ssh only from Public SG.",
-    ingress=[
-        aws.ec2.SecurityGroupIngressArgs(
-            protocol="-1",
-            from_port=0,
-            to_port=0,
-            security_groups=[bastion_sg.id]
-        )
-    ],
-    egress =[
-        aws.ec2.SecurityGroupEgressArgs(
-            protocol ="-1",
-            from_port=0,
-            to_port=0,
-            cidr_blocks = ["0.0.0.0/0"]
-        )
-    ],
-    tags={"Name":"private_sg"}
-)
+Host db-server
+    ProxyJump db-server
+    HostName {all_ips[1]}
+    User ubuntu
+    IdentityFile ~/.ssh/MyKeyPair.id_rsa
+"""
+    
+config_path = os.path.expanduser("~/.ssh/config")
+with open(config_path, "w") as config_file:
+    config_file.write(config_content)
 
-
-db_server = aws.ec2.Instance(
-    "db_server",
-    ami =ami_id ,
-    instance_type = "t2.micro",
-    subnet_id = private_subnet.id,
-    vpc_security_group_ids =[private_sg.id],
-    associate_public_ip_address = False,
-    key_name="MyKeyPair",
-    tags={"Name": "db_server"}
-)
-
-pulumi.export("db_server_ip", db_server.private_ip)
+all_ips = [bastion_server.public_ip, db_server.private_ip]
+pulumi.Output.all(*all_ips).apply(create_config_file)
